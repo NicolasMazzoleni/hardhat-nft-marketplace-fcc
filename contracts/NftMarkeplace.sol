ef084da1d5578019b6265e8bd40f7b6a2b2fe6d4
NICOLAS MAZZOLENI <mazzoleni.nicolas@gmail.com>
2023-02-16 14:57:24 +0100

    WIP

--- contracts/NftMarkeplace.sol
+++ contracts/NftMarkeplace.sol
@@ -1,15 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
 error NftMarketplace__PriceMustBeGreaterThanZero();
+error NftMarketplace__NotApprovedForMarketplace();
 
 contract NftMarketplace {
 
     // Main Functions
     function listItem(address nftContractAddress, uint256 ContractTokenId, uint256 price) external {
+        address currentAddress = address(this);
+
         if (price <= 0) {
-            revert NftMarketplace__PriceMustBeGreaterThanZero;
+            revert NftMarketplace__PriceMustBeGreaterThanZero();
         }
+
+        IERC721 nft = IERC721(nftContractAddress); // Using the Interface here
+        if (nft.getApproved(ContractTokenId) != currentAddress) {
+            revert NftMarketplace__NotApprovedForMarketplace();
+        }
+
     }
 }
 
